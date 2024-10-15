ed85a6db9553a2dc8337732e107b8fec3ab931ec
karnakarmarati <karnakarmaratikanni@gmail.com>
2024-10-15 22:08:59 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="App" >
+      <h2>MARATI.KARNAKAR</h2>
+      <h4>Email:<a href="">karnakarmaratikanni@kanni@gmail.com</a> </h4>
+      <h4>Contact Number:<a href="">+916302948618</a> </h4>
+      <h4>Github:<a href=""> https://github.com/karnakarkanni</a> </h4>
+      <h4>Linkdini:<a href="">https://www.linkedin.com/in/karnakar-marati-644097257/</a> </h4>
+      <hr></hr>
+
     </div>
+    <div id="k1">
+      <h2>Summary</h2>
+      <p>To work in a creative, challenging environment and achieve growth-oriented career in on organization, where I can 
+      contribute best to the growth of the organization by utilizing my technical skills, knowledge and functional experience</p>
+      <hr></hr>
+    </div>
+    <div id="k1">
+      <h2>EDUCATION</h2>
+      <p>Malla reddy college of engineering and technology,Hyderabad, TG 2024 
+      Bachelors in Electronics and communication engineering</p>
+      <hr></hr>
+    </div>
+    <div id="k1">
+      <h2>TECHNICAL SKILLS </h2>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>Java Script</li>
+      <li>Python</li>
+      <hr></hr>
+      
+    </div>
+    <div id="k1">
+      <h2>Projects</h2>
+      <h4>title:Safety Management system through smart poles </h4>
+<p>This proposal addresses the rising crime rates in India, particularly crimes against women, by introducing 
+safety management system through smart poles. These systems feature emergency button linked to police 
+control rooms via GSM networks and microprocessors. Integrated with cameras, GPS, and microcontrollers, 
+they enable rapid reporting of crimes and quick response from law enforcement</p>
+<h4>title:Lossless Image Compression Using Matlab </h4>
+<p>This process involves the elimination of various types of redundancies, such as coding redundancy, spatial 
+redundancy, and irrelevant information, ultimately leading to efficient data storage and transmission</p>
+<hr></hr>
+    </div>
+
+    <div id='k1' className="k2">
+      <h2>CERTIFICATIONS</h2>
+      <li> Certified in introduction to Programmig for Everybody in python by Coursera</li>
+      <li>Certified in Data Analysis with python by Cognitive certificates</li>
+      <li>ertified in Getting Started with Git and Github by Coursera</li>
+    </div>
+
+    </>
+
   );
 }
 
